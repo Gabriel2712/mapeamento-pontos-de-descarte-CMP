81bf044deffcce189f693a0f2a6d4db472f2b8b1
Gabriel2712 <gabrielcabral952@gmail.com>
2020-06-29 11:45:27 -0300

    adicionando icone para barra de endereço

--- i_topo.php
+++ i_topo.php
@@ -1,3 +1,4 @@
+<link rel="shortcut icon" href="https://img.icons8.com/doodle/48/000000/green-earth.png">
 <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
