57751455db64dc0fbbf76b8bb95bb0db1d11332b
Rafael Almeida <almeidaderafael@gmail.com>
2020-09-30 09:29:45 -0300

    alterando as informações da home

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="page-heading">
-            <h1>O PROJETO EM SI</h1><br>
-            <h4><span class="subheading">Quais os motivos, objetivos, e razões da criação?</span></h4>
+            <h1>DESCARTE DE RESÍDUOS EM CAMPINAS</h1><br>
+            <h4><span class="subheading">Pesquise o ponto de descarte de resíduos Eletrônicos e Recicláveis mais perto de você!</span></h4>
           </div>
         </div>
       </div>
@@ -28,9 +28,9 @@
   <div class="container">
     <div class="row">
       <div class="col-lg-8 col-md-10 mx-auto">
-        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSegundo o BBC News, a maioria dos brasileiros não sabem como funciona a reciclagem e não sabem quais os tipos de materiais plásticos, por exemplo, que podem ou não ser reaproveitados. Devido esta desinformação e da necessidade de se desfazer corretamente de tais dejetos produzidos tanto em âmbito familiar, quanto em âmbito social, além do desconhecimento de locais adequados para a realização destes descartes, o presente projeto teve como objetivo principal mapear pontos de descarte de materiais recicláveis e eletrônicos na cidade de Campinas, a fim de conscientizar e incentivar a população a reciclar. Levamos como propósito também, analisando por meio das nossas pesquisas, descobrir qual o real incentivo que é dado para as pessoas jogarem o lixo no local correto e separarem os materiais mais complexos (eletrônicos e recicláveis) dos menos complexos (lixo comum).
+        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspTendo como objetivo geral deste trabalho produzir um meio para consulta de pontos de descarte de resíduos recicláveis e eletrônicos na cidade de Campinas, embasado em pesquisas e considerando a tamanha necessidade do descarte correto destes dejetos citados, foi criado este site, a fim de auxiliar as pessoas a encontrarem locais de descarte de resíduos específicos (Eletrônicos e Recicláveis) na cidade de Campinas.  
         </p>
-        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspO projeto foi desenvolvido pelos estudantes Gabriel de Araújo Cabral, Ivy Cabral Barbosa, e Rafael de Almeida, matriculados  no 3º ano do Curso técnico de informática integrado ao Ensino Médio, do Instituto Federal de Ciência, Educação e Tecnologia de São Paulo - Campus Campinas, com o auxílio de professores formados na área de TI.
+        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspQuer nos auxiliar nesta causa? Envie novos pontos que podemos adicionar no nosso site, assim podemos nos manter sempre atualizados. Vá até a aba <a href="atualizacao.php" style="color: white">"Atualize-nos"</a> e envie sua proposta, com certeza será uma contribuição enorme para nós e para o meio ambiente!
         </p>
       </div>
     </div>
