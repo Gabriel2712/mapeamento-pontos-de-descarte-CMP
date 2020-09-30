9f90c3637c9cbb6a3c2d178371c083d9f52cb82e
ivycabral123 <almeidaderafael@gmail.com>
2020-09-30 18:36:59 -0300

    correção do menu na aba mapeamento

--- mapa.php
+++ mapa.php
@@ -19,32 +19,36 @@
 
 <body>
        <div id="mapid" style="position: absolute; float: left; top: 9%; height: 90%; width: 100%;  z-index: 1;">
-        <script>
-        var map =  L.map('mapid').setView([-22.906288, -47.082326], 14);
-    
-        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-        maxZoom: 25,
-        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
-          '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
-          'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        id: 'mapbox/streets-v11'
-      }).addTo(map);
-    
-          var marker = L.marker([-22.817304, -47.100553]).addTo(map);
-          marker.valueOf()._icon.style.filter = 'hue-rotate(150deg)';
-          marker.bindPopup("Resíduos: Eletrônicos e Recicláveis").openPopup();  
+         <script>
+          var map =  L.map('mapid').setView([-22.906288, -47.082326], 14);
+      
+          L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+          maxZoom: 25,
+          attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+            '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+            'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+          id: 'mapbox/streets-v11'
+        }).addTo(map);
+      
+            var marker = L.marker([-22.817304, -47.100553]).addTo(map);
+            marker.valueOf()._icon.style.filter = 'hue-rotate(150deg)';
+            marker.bindPopup("Resíduos: Eletrônicos e Recicláveis").openPopup();  
 
-          var marker = L.marker([-22.904054, -47.070088]).addTo(map);
-          marker.valueOf()._icon.style.filter = 'grayscale(100%)';
-          marker.bindPopup("Resíduos: Eletrônicos").openPopup();
+            var marker = L.marker([-22.904054, -47.070088]).addTo(map);
+            marker.valueOf()._icon.style.filter = 'grayscale(100%)';
+            marker.bindPopup("Resíduos: Eletrônicos").openPopup();
 
-          var marker = L.marker([-22.934027, -47.105675]).addTo(map);
-          marker.valueOf()._icon.style.filter = 'hue-rotate(290deg)';
-          marker.bindPopup("Resíduos: Recicláveis").openPopup(); 
-
-
+            var marker = L.marker([-22.934027, -47.105675]).addTo(map);
+            marker.valueOf()._icon.style.filter = 'hue-rotate(290deg)';
+            marker.bindPopup("Resíduos: Recicláveis").openPopup(); 
         </script>
        </div>
+  <!-- Bootstrap core JavaScript -->
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Custom scripts for this template -->
+  <script src="js/clean-blog.min.js"></script>
 
 </body>
 </html>
