d3a7e3f31ae14e9b8331f6b7276bb86367d6ec55
Gabriel2712 <gabrielcabral952@gmail.com>
2021-09-23 23:33:23 -0300

    pequenos ajustes

--- login.php
+++ login.php
@@ -10,12 +10,13 @@
 <body>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('https://cdn.pixabay.com/photo/2018/10/09/09/59/mobile-phone-3734545_960_720.jpg');">
+  <header class="masthead" style="background-image: url('img/lixoseletronicos-atualize.jpg');">
     <div class="container">
       <div class="row">
         <div class="col-lg-8 col-md-10 mx-auto">
           <div class="page-heading" style="padding:150px 0 100px">
-            <h1>Faça Login para continuar</h1><br>
+          <h1>FAÇA O LOGIN</h1><br>
+          <h4><span class="subheading">Assim você poderá contribuir com o bom funcionamento do sistema através de sugestões e feedbacks</span></h4><br>
           </div>
         </div>
       </div>
@@ -27,17 +28,19 @@
     <div>
       <div class="col-lg-8 mx-auto">
         <form name="sentMessage" id="loginForm" method="POST" action="bd_login.php">
-          <center><h2><p style="color: white">Login</p></h4></center>
+          <center><h4><p style="color: white">Login</p></h4></center>
           <div>
             <div>
-              <input type="email" class="customInput" placeholder="example@mail.com" name="email" required data-validation-required-message="Por gentileza, digite o endereço completo do ponto." style="background-color: white">
+              <label>Email:</label>
+                <input type="email" class="customInput" placeholder="example@mail.com" name="email" required data-validation-required-message="Por gentileza, digite o seu nome completo" style="background-color: white">
               <p class="help-block text-danger"></p>
             </div>
             <div>
-              <input type="password" class="customInput" placeholder="*********" name="password" required data-validation-required-message="Por gentileza, digite o endereço completo do ponto." style="background-color: white">
+              <label>Senha: </label>
+                <input type="password" class="customInput" placeholder="*********" name="password" required data-validation-required-message="Por gentileza, digite a sua senha" style="background-color: white">
               <p class="help-block text-danger"></p>
             </div>
-          <br>
+            <p align="center"><font size="2">Não possui conta? <a href ="cadastro.php" style="color: white">Clique para se cadastrar </a></font></p>
           <div class="form-group">
             <center><button type="submit" class="btn btn-primary" id="loginButton">Enviar</button></center>
           </div>
@@ -47,51 +50,7 @@
   </div>
   </div>
 
-  <hr>
-
-  <!-- Footer -->
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <ul class="list-inline text-center">
-            <li class="list-inline-item">
-              <a href="https://twitter.com/DescarteCmp">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="https://github.com/Gabriel2712/mapeamento-pontos-de-descarte-CMP">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-          </ul>
-          <p class="copyright text-muted">Copyright &copy; MapER 2021</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
+  <?php include 'i_rodape.php' ?>
 
 </body>
 
