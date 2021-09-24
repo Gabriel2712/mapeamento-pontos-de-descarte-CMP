95ebe159ab37c97850883ed7ee1bab46a1f728dd
Gabriel2712 <gabrielcabral952@gmail.com>
2021-09-23 23:32:39 -0300

    inserção de include e pequenos ajustes

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 <body>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('https://cdn.pixabay.com/photo/2018/05/04/20/18/architecture-3374883_960_720.jpg')">
+  <header class="masthead" style="background-image: url('img/campinas-home.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
@@ -36,51 +36,7 @@
     </div>
   </div>
 
-  <hr>
-
-  <!-- Footer -->
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <ul class="list-inline text-center">
-            <li class="list-inline-item">
-              <a href="https://twitter.com/DescarteCmp">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="https://github.com/Gabriel2712/mapeamento-pontos-de-descarte-CMP">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-          </ul>
-          <p class="copyright text-muted">Copyright &copy; MapER 2021</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
+  <?php include 'i_rodape.php' ?>
 
 </body>
 
--- sobre.php
+++ sobre.php
@@ -10,7 +10,7 @@
 <body>
 
   <!-- Page Header -->
-  <header class="masthead" style="background-image: url('https://c.pxhere.com/photos/fb/5e/pollution_industrial_plant_steel_mill_blast_furnace_hut_ruhr_area_north_rhine_westphalia_factory-765856.jpg!d')">
+  <header class="masthead" style="background-image: url('img/poluicao-sobre.jpg')">
     <div class="overlay"></div>
     <div class="container">
       <div class="row">
@@ -30,57 +30,13 @@
       <div class="col-lg-8 col-md-10 mx-auto">
         <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspSegundo o BBC News, a maioria dos brasileiros não sabem como funciona a reciclagem e não sabem quais os tipos de materiais plásticos, por exemplo, que podem ou não ser reaproveitados. Devido esta desinformação e da necessidade de se desfazer corretamente de tais dejetos produzidos tanto em âmbito familiar, quanto em âmbito social, além do desconhecimento de locais adequados para a realização destes descartes, o presente projeto teve como objetivo principal mapear pontos de descarte de materiais recicláveis e eletrônicos na cidade de Campinas, a fim de conscientizar e incentivar a população a reciclar. Levamos como propósito também, analisando por meio das nossas pesquisas, descobrir qual o real incentivo que é dado para as pessoas jogarem o lixo no local correto e separarem os materiais mais complexos (eletrônicos e recicláveis) dos menos complexos (lixo comum).
         </p>
-        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspO projeto foi desenvolvido pelos estudantes Gabriel de Araújo Cabral, Ivy Cabral Barbosa, e Rafael de Almeida, matriculados  no 3º ano do Curso técnico de informática integrado ao Ensino Médio, do Instituto Federal de Ciência, Educação e Tecnologia de São Paulo - Campus Campinas, com o auxílio de professores formados na área de TI.
+        <p align="justify">&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbspO projeto foi desenvolvido pelos estudantes Gabriel de Araújo Cabral, Ivy Cabral Barbosa, e Rafael de Almeida, matriculados no 4º ano do Curso técnico de informática integrado ao Ensino Médio, do Instituto Federal de Ciência, Educação e Tecnologia de São Paulo - Campus Campinas, com o auxílio de professores formados na área de TI.
         </p>
       </div>
     </div>
   </div>
 
-  <hr>
-
-  <!-- Footer -->
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 col-md-10 mx-auto">
-          <ul class="list-inline text-center">
-            <li class="list-inline-item">
-              <a href="https://twitter.com/DescarteCmp">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-twitter fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="#">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-facebook-f fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-            <li class="list-inline-item">
-              <a href="https://github.com/Gabriel2712/mapeamento-pontos-de-descarte-CMP">
-                <span class="fa-stack fa-lg">
-                  <i class="fas fa-circle fa-stack-2x"></i>
-                  <i class="fab fa-github fa-stack-1x fa-inverse"></i>
-                </span>
-              </a>
-            </li>
-          </ul>
-          <p class="copyright text-muted">Copyright &copy; MapER 2021</p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-  <!-- Custom scripts for this template -->
-  <script src="js/clean-blog.min.js"></script>
+  <?php include 'i_rodape.php' ?>
 
 </body>
 
