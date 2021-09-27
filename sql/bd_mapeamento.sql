f10aab7a0cbdfa8dc9511570beb3cd07eef3831a
Gabriel2712 <gabrielcabral952@gmail.com>
2021-09-26 22:30:58 -0300

    atualizando clausula do banco

--- sql/bd_mapeamento.sql
+++ sql/bd_mapeamento.sql
@@ -1,17 +1,23 @@
 create database if not exists bd_mapeamento;
 use bd_mapeamento;
 
-create table if not exists tb_visitante(
-  cod_vst int primary key auto_increment,
-  nome_vst varchar(100) not null,
-  email_vst varchar(30),
-  tel_vst varchar(30)
-  );
+create table if not exists tb_usuario (
+  id_usuario int primary key auto_increment,
+  nome_usuario varchar(200) NOT NULL,
+  telefone_usuario varchar(30),
+  email_usuario varchar(250) NOT NULL,
+  senha_usuario varchar(200) NOT NULL
+)ENGINE=InnoDB;
 
 create table if not exists tb_ponto(
-  cod_ponto int primary key auto_increment,
-  end_ponto varchar(150),
+  id_ponto int primary key auto_increment,
+  end_ponto varchar(250),
   tel_ponto varchar(30),
-  descarte_ponto varchar(15),
-  desc_ponto varchar(200)
+  descarte_ponto varchar(30),
+  desc_ponto varchar(250),
+  id_usuario int,
+  FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
+describe tb_usuario;
+show tables;
+select *from tb_usuario;
