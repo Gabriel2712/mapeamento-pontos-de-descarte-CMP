2d364535f09e1ba384dfd2569a7212c8c75564b9
Gabriel2712 <gabrielcabral952@gmail.com>
2021-11-03 13:42:00 -0300

    Ajustes no banco, com alteração de tb_ponto para tb_sugestao

--- sql/bd_mapeamento.sql
+++ sql/bd_mapeamento.sql
@@ -1,22 +1,23 @@
-
 create database if not exists bd_mapeamento;
 use bd_mapeamento;
 
 create table if not exists tb_usuario (
-  id_usuario int primary key auto_increment,
+  id_usuario int auto_increment primary key,
   nome_usuario varchar(200) NOT NULL,
-  telefone_usuario varchar(30),
+  telefone_usuario varchar(30) NOT NULL,
   email_usuario varchar(250) NOT NULL,
   senha_usuario varchar(200) NOT NULL,
+  data_cadastro date NOT NULL,
   tipo_usuario varchar(20) NOT NULL
 );
 
-create table if not exists tb_ponto(
-  id_ponto int primary key auto_increment,
-  end_ponto varchar(250),
+create table if not exists tb_sugestao(
+  id_sugestao int auto_increment primary key,
+  end_ponto varchar(250) NOT NULL,
   tel_ponto varchar(30),
-  descarte_ponto varchar(30),
-  desc_ponto varchar(250),
-  id_usuario int,
+  descarte_ponto varchar(30) NOT NULL,
+  desc_sugestao varchar(250) NOT NULL,
+  data_sugestao date NOT NULL,
+  id_usuario int NOT NULL,
   FOREIGN KEY (id_usuario) REFERENCES tb_usuario(id_usuario)
 );
