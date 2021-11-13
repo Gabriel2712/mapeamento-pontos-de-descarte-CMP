6d8e9a16aea68e6ef971518610747236506471e2
Gabriel2712 <gabrielcabral952@gmail.com>
2021-11-12 21:54:40 -0300

    Adicionando opção de validar/alterar status de sugestão

---
+++ bd_modifica_status.php
@@ -0,0 +1,36 @@
+<?php 
+    include 'include/valida_sessao_admin.php'
+ ?>
+<!DOCTYPE html>
+  <head>
+  	<title>Sugestões</title>
+		<?php include 'include/i_topo.php'?>
+  </head>
+  <body>
+    <?php include 'include/i_topo_admin.php' ?>
+		<div class="col-8">
+			<div id="conteudo_adm">
+            <?php
+                include 'include/conecta.php';
+                include 'include/busca_pesquisa.php';
+                if ($criterio == 'em_analise'){
+                    $query = "UPDATE tb_sugestao SET status_sugestao = 'em análise' WHERE id_sugestao = $chave";
+                }else if ($criterio == 'aceita'){
+                    $query = "UPDATE tb_sugestao SET status_sugestao = 'aceita' WHERE id_sugestao = $chave";
+                }else if ($criterio == 'recusada'){
+                    $query = "UPDATE tb_sugestao SET status_sugestao = 'recusada' WHERE id_sugestao = $chave";
+                }
+                $result = mysqli_query($db, $query);
+                
+                if(mysqli_affected_rows($db)>0){
+                echo 'Informação alterada com sucesso<br><u><a href= "adm_sugestoes.php" style="color: darkblue">Voltar<a></u>';
+                }
+                mysqli_close($db);
+            ?>
+        </div>
+    </div>
+    </div>
+
+  </body>
+  	<?php include 'include/i_rodape.php' ?>
+</html>
